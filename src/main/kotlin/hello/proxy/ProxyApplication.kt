597f24259312eaa2cb14b4b73ec5241e429c9a34
heosangbeom <luv_3_u@naver.com>
2024-12-19 10:32:23 +0900

    ✨feat(aop): aop-v1
    - aop-v1

--- src/main/kotlin/hello/proxy/ProxyApplication.kt
+++ src/main/kotlin/hello/proxy/ProxyApplication.kt
@@ -11,6 +11,7 @@ import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig
 import hello.proxy.config.v5_autoproxy.AutoProxyConfig
+import hello.proxy.config.v6_aop.AopConfig
 import hello.proxy.trace.logtrace.LogTrace
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -26,7 +27,8 @@ import org.springframework.context.annotation.Import
 //@Import(ProxyFactoryConfigV1::class, LogTraceConfig::class)
 //@Import(ProxyFactoryConfigV2::class, LogTraceConfig::class)
 //@Import(BeanPostProcessorConfig::class, LogTraceConfig::class)
-@Import(AutoProxyConfig::class,LogTraceConfig::class)
+//@Import(AutoProxyConfig::class,LogTraceConfig::class)
+@Import(AopConfig::class,LogTraceConfig::class)
 @SpringBootApplication(scanBasePackages = ["hello.proxy.app.v3"])
 class ProxyApplication
 
